b537b836fcfc1f74ffb1115ed9e0654818004d62
Beatriz-Sanchez <40073484+Beatriz-Sanchez@users.noreply.github.com>
2021-12-20 13:02:04 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,11 +1,16 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
 
+var engine, world;
 
 function setup() {
-  
+  engine = Engine.create();
+  world = engine.world;
 }
 
-function draw() 
-{
-  
+function draw() {
+  Engine.update(engine);
 }
 
