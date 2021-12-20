3bc37b6d09efea41acae6302bd9aa8013d5688a1
Beatriz-Sanchez <40073484+Beatriz-Sanchez@users.noreply.github.com>
2021-12-20 13:06:37 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -4,13 +4,37 @@ const Bodies = Matter.Bodies;
 const Body = Matter.Body;
 
 var engine, world;
+var bola, solo;
 
 function setup() {
+  createCanvas(400,400);
   engine = Engine.create();
   world = engine.world;
+
+  var bola_options = {
+    frictionAir: 0.01,
+    restitution: 1
+  };
+
+  bola = Bodies.circle(200,30,30, bola_options);
+  World.add(world, bola);
+
+  var solo_options = {
+    isStatic: true
+  };
+
+  solo = Bodies.rectangle(200,400,400,40, solo_options);
+  World.add(world, solo);
 }
 
-function draw() {
+function draw() 
+{
+  background("black");
   Engine.update(engine);
-}
 
+  ellipseMode(RADIUS);
+  ellipse(bola.position.x, bola.position.y,30);
+
+  rectMode(CENTER);
+  rect(solo.position.x, solo.position.y, 400,40);
+}
