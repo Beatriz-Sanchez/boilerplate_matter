08c699a07fa16a61cf281e000c522571d2adfe93
Beatriz-Sanchez <40073484+Beatriz-Sanchez@users.noreply.github.com>
2022-02-24 18:20:55 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -6,11 +6,13 @@ const Body = Matter.Body;
 var engine, world;
 
 function setup() {
+  createCanvas(400,400);
   engine = Engine.create();
   world = engine.world;
 }
 
 function draw() {
+  background("black")
   Engine.update(engine);
 }
 
